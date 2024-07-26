86b388a21e3d573620bdbad7a0cd05d7e8b4c7d6
juan <jeandarkmaknae@gmail.com>
2024-07-26 17:43:29 -0400

    crud-netcore-api-empleado

---
+++ Services/Contrato/IDepartamentoService.cs
@@ -0,0 +1,10 @@
+﻿
+using BackEndApiEmpleado.Models;
+
+namespace BackEndApiEmpleado.Services.Contrato
+{
+    public interface IDepartamentoService
+    {
+        Task<List<Departamento>> GetList();
+    }
+}
---
+++ Services/Implementacion/DepartamentoService.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using BackEndApiEmpleado.Models;
+using BackEndApiEmpleado.Services.Contrato;
+
+namespace BackEndApiEmpleado.Services.Implentacion
+{
+    
+    public class DepartamentoService: IDepartamentoService
+    {
+        private DbempleadoContext _dbContext;
+
+
+        public DepartamentoService(DbempleadoContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Departamento>> GetList()
+        {
+            try { 
+                List<Departamento> lista = new List<Departamento>();
+                lista = await _dbContext.Departamentos.ToListAsync();
+                return lista;
+            }
+            catch(Exception ex) {
+                throw ex;
+            }
+        }
+    }
+}
